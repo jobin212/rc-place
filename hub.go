804dc9c37f40765cc9bd7602af924039daa6f165
Miccah Castorina <m.castorina93@gmail.com>
2022-03-16 19:37:39 -0500

    Track changes to board in the server

--- hub.go
+++ hub.go
@@ -4,6 +4,17 @@
 
 package main
 
+import (
+	"errors"
+	"log"
+	"strconv"
+	"strings"
+)
+
+// boardSize is the width and height of the board, matching the table
+// size in home.html
+const boardSize = 100
+
 // Hub maintains the set of active clients and broadcasts messages to the
 // clients.
 type Hub struct {
@@ -18,15 +29,30 @@ type Hub struct {
 
 	// Unregister requests from clients.
 	unregister chan *Client
+
+	// board is an in-memory representation of the board
+	// where each entry is a javascript color
+	board [][]string
 }
 
 func newHub() *Hub {
-	return &Hub{
+	hub := &Hub{
 		broadcast:  make(chan []byte),
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
 		clients:    make(map[*Client]bool),
+		board:      make([][]string, boardSize),
 	}
+
+	// initialize board
+	for i := 0; i < len(hub.board); i++ {
+		hub.board[i] = make([]string, boardSize)
+		for j := 0; j < boardSize; j++ {
+			hub.board[i][j] = "cornflowerblue"
+		}
+	}
+
+	return hub
 }
 
 func (h *Hub) run() {
@@ -40,6 +66,10 @@ func (h *Hub) run() {
 				close(client.send)
 			}
 		case message := <-h.broadcast:
+			// parse and set color in memory
+			if err := h.parseAndSave(message); err != nil {
+				log.Println(err)
+			}
 			for client := range h.clients {
 				select {
 				case client.send <- message:
@@ -50,4 +80,36 @@ func (h *Hub) run() {
 			}
 		}
 	}
+}
+
+// parseAndSave parses a message into x, y, and color and saves it to
+// the board
+func (h *Hub) parseAndSave(message []byte) error {
+	s := string(message)
+	parts := strings.Split(s, " ")
+
+	if len(parts) < 3 {
+		// do nothing if we don't have enough information from the message
+		return errors.New("malformed message")
+	}
+
+	x, y, color := parts[0], parts[1], parts[2]
+	var xPos, yPos int
+	var err error
+
+	// convert to integers
+	if xPos, err = strconv.Atoi(x); err != nil {
+		return err
+	}
+	if yPos, err = strconv.Atoi(y); err != nil {
+		return err
+	}
+
+	// check bounds
+	if yPos < 0 || xPos < 0 || yPos >= len(h.board) || xPos >= len(h.board[yPos]) {
+		return errors.New("out of bounds")
+	}
+
+	h.board[yPos][xPos] = color
+	return nil
 }
