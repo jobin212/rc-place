1a109056373f5a26d7e9835cc2ea378d6328fb59
Miccah Castorina <m.castorina93@gmail.com>
2022-03-16 22:49:21 -0500

    Add basic oauth flow

--- go.mod
+++ go.mod
@@ -3,3 +3,11 @@ module rc-place
 go 1.17
 
 require github.com/gorilla/websocket v1.5.0
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/oauth2 v0.0.0-20220309155454-6242fa91716a // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,14 +5,22 @@
 package main
 
 import (
+	"crypto/rand"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"golang.org/x/oauth2"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
+
+// stateMap is used for oauth redirection to protect users from CSRF
+// attacks. See https://pkg.go.dev/golang.org/x/oauth2#Config.AuthCodeURL
+var stateMap = map[string]string{}
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
@@ -24,6 +32,19 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
+	// authenticate user
+	state := r.FormValue("state")
+	code := r.FormValue("code")
+	if state == "" || state != stateMap[r.RemoteAddr] {
+		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+		return
+	}
+	if code == "" {
+		// access denied
+		http.Error(w, "Unoauthorized", http.StatusUnauthorized)
+		return
+	}
+	// TODO: should we actually try to use the token to see if it's legitimate?
 	http.ServeFile(w, r, "home.html")
 }
 
@@ -38,8 +59,9 @@ func serveTile(hub *Hub, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	defer r.Body.Close()
+	// TODO: authentication
 
+	defer r.Body.Close()
 	type jsonBody struct {
 		X     int    `json:"x"`
 		Y     int    `json:"y"`
@@ -53,11 +75,60 @@ func serveTile(hub *Hub, w http.ResponseWriter, r *http.Request) {
 	hub.broadcast <- []byte(fmt.Sprintf("%d %d %s", j.X, j.Y, j.Color))
 }
 
+func serveLogin(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/login" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// generate random string
+	state, err := generateRandomString()
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	// TODO: use cookie value instead of RemoteAddr
+	stateMap[r.RemoteAddr] = state
+	conf := &oauth2.Config{
+		RedirectURL:  os.Getenv("OAUTH_REDIRECT"),
+		ClientID:     os.Getenv("OAUTH_CLIENT_ID"),
+		ClientSecret: os.Getenv("OAUTH_CLIENT_SECRET"),
+		Scopes:       []string{},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://recurse.com/oauth/authorize",
+			TokenURL: "https://recurse.com/oauth/token",
+		},
+	}
+	url := conf.AuthCodeURL(stateMap[r.RemoteAddr], oauth2.AccessTypeOnline)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
 func main() {
 	flag.Parse()
+
+	// check oauth environment variables
+	abort := false
+	for _, env := range []string{"OAUTH_REDIRECT", "OAUTH_CLIENT_ID", "OAUTH_CLIENT_SECRET"} {
+		if os.Getenv(env) == "" {
+			log.Println("Required environment variable missing:", env)
+			abort = true
+		}
+	}
+	if abort {
+		log.Println("Aborting")
+		os.Exit(1)
+	}
+
 	hub := newHub()
 	go hub.run()
 	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/login", serveLogin)
 	http.HandleFunc("/tile", func(w http.ResponseWriter, r *http.Request) {
 		serveTile(hub, w, r)
 	})
@@ -68,4 +139,15 @@ func main() {
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+// generateRandomString generates a random hex string
+func generateRandomString() (string, error) {
+	n := 8
+	b := make([]byte, n)
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+	s := fmt.Sprintf("%x", b)
+	return s, nil
 }
