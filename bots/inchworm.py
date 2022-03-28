de0f3589df3ac4e06954630eca0457acb6186a16
jobin212 <jobin212@gmail.com>
2022-03-28 15:56:21 -0400

    create inchworm

---
+++ bots/inchworm.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+from threading import local
+import requests
+import os
+import time
+
+prod_url = "https://rc-place.fly.dev/tile"
+local_url = "http://localhost:8080/tile"
+
+timeout = 1.1
+
+def set_tile(x, y, color):
+    data = { "x": x, "y": y, "color": color}
+    headers = { 'Authorization': 'Bearer ' + os.getenv("PERSONAL_ACCESS_TOKEN"), 'Content-Type': "application/json"}
+    
+    resp = requests.post(prod_url, json=data, headers=headers)
+    
+    if resp.status_code == 200:
+        print("Tile placed at (%s,%s)" % (x, y))
+    else:
+        print("Tile not placed at (%s,%s) | Status code: %d | Message: %s" % (x, y, resp.status_code, resp.text))
+
+def main():
+    # create inchworm
+    headX, headY, inchworm_length, inchworm_color, default_color = 75, 50, 5, "lime", "cornflowerblue"
+    for y in range(inchworm_length):
+        set_tile(headX, headY + y, inchworm_color)
+        time.sleep(timeout)
+
+    # move snake :)
+    while True:
+        set_tile(headX, headY + 1, inchworm_color)
+        time.sleep(timeout)
+        set_tile(headX, (headY-inchworm_length) % 100, default_color)
+        time.sleep(timeout)
+        headY += 1
+        headY %= 100
+
+if __name__ == "__main__":
+    main()
