c84fcc68f73cd99713e7b8e4921cd06eabad9220
jobin212 <jobin212@gmail.com>
2022-03-30 16:45:29 -0400

    revert inchworm

--- bots/inchworm.py
+++ bots/inchworm.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 
-from email.policy import default
 from threading import local
 import requests
 import os
 import time
-import sys
 
 prod_url = "https://rc-place.fly.dev/tile"
 local_url = "http://localhost:8080/tile"
-default_color = "cornflowerblue"
 
 # Wait period between API calls in seconds
-timeout = 0.011
+timeout = 1.1
 
 def set_tile(x, y, color):
     data = { "x": x, "y": y, "color": color}
@@ -25,48 +22,20 @@ def set_tile(x, y, color):
     else:
         print("Tile not placed at (%s,%s) | Status code: %d | Message: %s" % (x, y, resp.status_code, resp.text))
 
-def get_tile(x, y):
-    headers = { 'Authorization': 'Bearer ' + os.getenv("PERSONAL_ACCESS_TOKEN"), 'Content-Type': "application/json"}
-
-    url = "{0}?x={1}&y={2}".format(prod_url, x, y)
-    resp = requests.get(url, headers=headers)
-
-    if resp.status_code == 200:
-        resp_body = resp.json()
-        color = resp_body["color"]
-        return color
-    else:
-        print("GET failed")
-        return default_color
-
-def main(args):
-    headX, headY = 76, 25
-    try:
-        headX, headY = int(args[0]), int(args[1])
-    except:
-        print("Invalid x, y values given. Starting inchworm at {0},{1}", headX, headY)
-
+def main():
     # create inchworm
-    inchworm_length, inchworm_color = 5, "burnt-orange"
-
-    for y in reversed(range(inchworm_length)):
-        set_tile(headX, (headY - y)%100, inchworm_color)
+    headX, headY, inchworm_length, inchworm_color, default_color = 75, 50, 5, "lime", "cornflowerblue"
+    for y in range(inchworm_length):
+        set_tile(headX, headY + y, inchworm_color)
         time.sleep(timeout)
 
     # move inchworm
     while True:
-        color = get_tile(headX, headY + 1)
-
         set_tile(headX, headY + 1, inchworm_color)
         time.sleep(timeout)
-        if color != default_color:
-            # inchworm just ate a non-default block and can now grow
-            inchworm_length += 1
-        else:
-            # move the tail of the inchworm
-            set_tile(headX, (headY-inchworm_length) % 100, default_color)
-            time.sleep(timeout)
+        set_tile(headX, (headY-inchworm_length) % 100, default_color)
+        time.sleep(timeout)
         headY = (headY + 1) % 100
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
