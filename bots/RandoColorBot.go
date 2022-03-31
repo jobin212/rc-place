b95eab1dcd55c1d27d1478a4dc1c71fed1143986
jobin212 <jobin212@gmail.com>
2022-03-31 11:56:09 -0700

    revert bot changes

---
+++ bots/RandoColorBot.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+const (
+	apiUrl = "https://rc-place.fly.dev"
+	max_x  = 100
+	max_y  = 100
+)
+
+var BearerToken string = os.Getenv("PERSONAL_ACCESS_TOKEN")
+
+type PixelStruct struct {
+	Color       string    `json:"color"`
+	X           int       `json:"x"`
+	Y           int       `json:"y"`
+	LastUpdated time.Time `json:"lastUpdated"`
+	LastEditor  string    `json:"lastEditor"`
+}
+
+func main() {
+	checkToken()
+	colorsAvailable := []string{"black", "forest", "green", "lime", "blue", "cornflowerblue", "sky", "cyan", "red", "burnt-orange", "orange", "yellow", "purple", "hot-pink", "pink", "white"}
+	generalColor := getGeneralBoardColor()
+	fmt.Println("Seems like most of the pixels are: ", generalColor)
+
+	randomColor := randomColorPicker(colorsAvailable)
+	fmt.Println("Changing pixels to: ", randomColor)
+	if generalColor == randomColor {
+		fmt.Println("The general color is the same as the random color, not changing anything.")
+		return
+	}
+
+	// get user input for number of pixels to update
+	var numPixelsToUpdate int
+	fmt.Println("How many pixels would you like to update?")
+	fmt.Scanf("%d", &numPixelsToUpdate)
+	if numPixelsToUpdate > max_x*max_y {
+		fmt.Println("That's too many pixels, I'm only going to update", max_x*max_y, "pixels.")
+		numPixelsToUpdate = max_x * max_y
+	}
+
+	fmt.Println("Updating pixels...")
+
+	updateRandomPixels(generalColor, randomColor, numPixelsToUpdate)
+}
+
+func checkToken() {
+	if BearerToken == "" {
+		fmt.Println("Please set the environment variable PERSONAL_ACCESS_TOKEN")
+		os.Exit(1)
+	}
+}
+
+// function that takes an originating color and updates random pixels to a new given color if it matches the originating color
+func updateRandomPixels(originatingColor string, newColor string, numPixelsToUpdate int) {
+	for i := 0; i < numPixelsToUpdate; i++ {
+		x, y := randomCoordinatePicker()
+		color, _ := getPixelState(x, y)
+		if color == originatingColor {
+			updatePixelState(x, y, newColor)
+		}
+	}
+}
+
+// This is a rough estimate of the general color of the board.
+// TODO: get a batch state of the board?
+func getGeneralBoardColor() string {
+	// not the prettiest
+	var colorCount = map[string]int{
+		"black":          0,
+		"forest":         0,
+		"green":          0,
+		"lime":           0,
+		"blue":           0,
+		"cornflowerblue": 0,
+		"sky":            0,
+		"cyan":           0,
+		"red":            0,
+		"burnt-orange":   0,
+		"orange":         0,
+		"yellow":         0,
+		"purple":         0,
+		"hot-pink":       0,
+		"pink":           0,
+		"white":          0,
+	}
+
+	sampleSize := 20 // 2% of the board, but is pretty good
+	for i := 0; i < sampleSize; i++ {
+		color, _ := getPixelState(randomCoordinatePicker())
+		colorCount[color]++
+	}
+
+	// find the most common color
+	var mostCommonColor string
+	var mostCommonCount int
+	for color, count := range colorCount {
+		if count > mostCommonCount {
+			mostCommonColor = color
+			mostCommonCount = count
+		}
+	}
+
+	return mostCommonColor
+}
+
+func getPixelState(x int, y int) (string, error) {
+	url := fmt.Sprintf("%s/tile?x=%d&y=%d", apiUrl, x, y)
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", BearerToken))
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	defer res.Body.Close()
+
+	// parse body for color
+	var p PixelStruct
+	err = json.NewDecoder(res.Body).Decode(&p)
+	return (p.Color), err
+}
+
+func updatePixelState(x int, y int, color string) error {
+	url := fmt.Sprintf("%s/tile", apiUrl)
+	method := "POST"
+
+	bodyString := fmt.Sprintf(`{"x": %d, "y": %d, "color": "%s"}`, x, y, color)
+
+	payload := strings.NewReader(bodyString)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", BearerToken))
+
+	_, err = client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return err
+}
+
+// function that given a string array returns a random string from the array
+func randomColorPicker(strArray []string) string {
+	rand.Seed(time.Now().UnixNano())
+	randomIndex := rand.Intn(len(strArray))
+	return strArray[randomIndex]
+}
+
+// function that gives a random x and y coordinate given the max x and y
+func randomCoordinatePicker() (int, int) {
+	rand.Seed(time.Now().UnixNano())
+	x := rand.Intn(max_x)
+	y := rand.Intn(max_y)
+	return x, y
+}
--- bots/inchworm.py
+++ bots/inchworm.py
@@ -1,98 +1,41 @@
 #!/usr/bin/python3
 
-from email.policy import default
 from threading import local
 import requests
 import os
 import time
-import sys
 
-# url = "https://rc-place.fly.dev/tile"
-url = "http://localhost:8080"
-default_color = "cornflowerblue"
+prod_url = "https://rc-place.fly.dev/tile"
+local_url = "http://localhost:8080/tile"
 
 # Wait period between API calls in seconds
-timeout = 0.011
+timeout = 1.1
 
 def set_tile(x, y, color):
     data = { "x": x, "y": y, "color": color}
     headers = { 'Authorization': 'Bearer ' + os.getenv("PERSONAL_ACCESS_TOKEN"), 'Content-Type': "application/json"}
-    formatted_url = "{0}/tile".format(url)
     
-    resp = requests.post(formatted_url, json=data, headers=headers)
+    resp = requests.post(prod_url, json=data, headers=headers)
     
     if resp.status_code == 200:
         print("Tile placed at (%s,%s)" % (x, y))
     else:
         print("Tile not placed at (%s,%s) | Status code: %d | Message: %s" % (x, y, resp.status_code, resp.text))
 
-def get_tile(x, y):
-    headers = { 'Authorization': 'Bearer ' + os.getenv("PERSONAL_ACCESS_TOKEN"), 'Content-Type': "application/json"}
-
-    formatted_url = "{0}/{1}?x={2}&y={3}".format(url, "tile", x, y)
-    resp = requests.get(formatted_url, headers=headers)
-
-    if resp.status_code == 200:
-        resp_body = resp.json()
-        color = resp_body["color"]
-        return color
-    else:
-        print("GET failed")
-        return default_color
-
-def get_tiles():
-    headers = { 'Authorization': 'Bearer ' + os.getenv("PERSONAL_ACCESS_TOKEN")}
-    formtted_url = "{0}/{1}".format(url, "tiles")
-    resp = requests.get(formtted_url, headers=headers)
-
-    if resp.status_code == 200:
-        resp_body = resp.json()
-        return resp_body["tiles"]
-    else:
-        print("GET failed")
-        return None
-
-def get_most_popular_color():
-    tiles = get_tiles()
-
-    if tiles is not None:
-        for x in len(tiles):
-            for y in len(tiles[x]):
-                print(tiles[y][x])
-
-    return default_color
-
-def main(args):
-    # grow longer only when we see the least popular color
-    default_color = get_most_popular_color()
-
-    headX, headY = 76, 25
-    try:
-        headX, headY = int(args[0]), int(args[1])
-    except:
-        print("Invalid x, y values given. Starting inchworm at {0},{1}", headX, headY)
-
+def main():
     # create inchworm
-    inchworm_length, inchworm_color = 5, "burnt-orange"
-
-    for y in reversed(range(inchworm_length)):
-        set_tile(headX, (headY - y)%100, inchworm_color)
+    headX, headY, inchworm_length, inchworm_color, default_color = 75, 50, 5, "lime", "cornflowerblue"
+    for y in range(inchworm_length):
+        set_tile(headX, headY + y, inchworm_color)
         time.sleep(timeout)
 
     # move inchworm
     while True:
-        color = get_tile(headX, headY + 1)
-
         set_tile(headX, headY + 1, inchworm_color)
         time.sleep(timeout)
-        if color != default_color:
-            # inchworm just ate a non-default block and can now grow
-            inchworm_length += 1
-        else:
-            # move the tail of the inchworm
-            set_tile(headX, (headY-inchworm_length) % 100, default_color)
-            time.sleep(timeout)
+        set_tile(headX, (headY-inchworm_length) % 100, default_color)
+        time.sleep(timeout)
         headY = (headY + 1) % 100
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
